275c528cbcba5363599810ec4fa2b741d47642d5
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-07-24 08:38:32 +0200

    cli (lib) introduced

---
+++ lib/ir_proxy/cli.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+# Copyright (C) 2017-2019 Dimitri Arrigoni <dimitri@arrigoni.me>
+# License GPLv3+: GNU GPL version 3 or later
+# <http://www.gnu.org/licenses/gpl.html>.
+# This is free software: you are free to change and redistribute it.
+# There is NO WARRANTY, to the extent permitted by law.
+
+require_relative '../ir_proxy'
+
+# Describe main CLI.
+#
+# Mostly an entry-point for CLI.
+class IrProxy::Cli
+  def call
+    puts 'Hello world'
+
+    return 0
+  end
+
+  class << self
+    def call
+      self.new.call
+    end
+  end
+end
