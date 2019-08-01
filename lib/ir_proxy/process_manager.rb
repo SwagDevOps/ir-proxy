77418b970967897eac0a8c024ae56a2ae3a88cfe
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-08-01 10:04:05 +0200

    state (process_manager) introduced

--- lib/ir_proxy/process_manager.rb
+++ lib/ir_proxy/process_manager.rb
@@ -17,6 +17,7 @@ class IrProxy::ProcessManager
   # @formatter:off
   {
     Shell: 'shell',
+    State: 'state',
   }.each { |s, fp| autoload(s, "#{__dir__}/process_manager/#{fp}") }
   # @formatter:on
 
@@ -24,38 +25,31 @@ class IrProxy::ProcessManager
   #
   # @return [Integer]
   def pgid
-    Process.getpgid($PROCESS_ID)
+    state.pgid
   end
 
   # Execute given `args` as subprocess command line.
   #
   # @param [String] args
   def call(*args)
-    self.clean.tap do
-      self.thread do
-        fork { sh(*args) }.tap { |pid| pids.push(pid) }
-      end.join
-    end
-  end
+    self.thread do
+      fork { sh(*args) }.tap do |pid|
+        Process.detach(pid)
+        self.state.push(pid)
+      end
+    end.join
 
-  # @return [self]
-  def clean
-    self.tap do
-      self.pids.keep_if { |pid| alive?(pid) }
-    end
+    self
   end
 
   class << self
     def call
       yield(instance)
 
-      instance.__send__(:clean).__send__(:pids).tap do |pids|
-        until pids.empty?
-          sleep(0.1)
-          pp(pids)
-        end
+      instance.__send__(:state).tap do |state|
+        sleep(0.1) until state.clean.empty?
+        exit(0)
       end
-      exit(0)
     end
   end
 
@@ -69,9 +63,7 @@ class IrProxy::ProcessManager
   # Terminate process and subprocesses.
   def terminate
     warn("Terminating (#{$PROCESS_ID})...")
-    Timeout.timeout(self.terminate_timeout) do
-      pids.each { |pid| self.kill(pid) } until clean.pids.empty?
-    end
+    self.state.clear
     exit(0)
   rescue Timeout::Error
     Process.kill(:HUP, -self.pgid)
@@ -79,18 +71,15 @@ class IrProxy::ProcessManager
 
   protected
 
-  # @return [Array<Integer>]
-  attr_accessor :pids
-
-  # @type [Hash{String => String}]
+  # @return [Hash{String => String}]
   attr_accessor :env
 
-  attr_accessor :terminate_timeout
+  # @return [State]
+  attr_accessor :state
 
   def initialize
-    self.pids = []
     self.env = ENV.to_h.freeze
-    self.terminate_timeout = 5
+    self.state = State.new(timeout: 5)
 
     [:INT, :TERM].each do |sign|
       Signal.trap(sign) { self.terminate }
@@ -102,19 +91,6 @@ class IrProxy::ProcessManager
     Thread.abort_on_exception = true
 
     Thread.new(&Proc.new)
-  end
-
-  # @return [Boolean]
-  def alive?(pid)
-    Process.waitpid(pid, Process::WNOHANG) and Process.getpgid(pid) == pgid
-  rescue Errno::ESRCH, Errno::ECHILD
-    false
-  end
-
-  def kill(pid)
-    Process.kill(:INT, pid) if Process.getpgid(pid) == pgid
-  rescue Errno::ESRCH, Errno::ECHILD
-    false
   end
 
   # @return [Shell]
