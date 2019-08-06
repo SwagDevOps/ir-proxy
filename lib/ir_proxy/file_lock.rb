4a4b2f1e07f58b27b03904faec4e68f795d22f18
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-08-06 19:22:31 +0200

    file_lock (lib) introduced

---
+++ lib/ir_proxy/file_lock.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+# Copyright (C) 2017-2019 Dimitri Arrigoni <dimitri@arrigoni.me>
+# License GPLv3+: GNU GPL version 3 or later
+# <http://www.gnu.org/licenses/gpl.html>.
+# This is free software: you are free to change and redistribute it.
+# There is NO WARRANTY, to the extent permitted by law.
+
+require_relative '../ir_proxy'
+require 'sys/proc'
+
+# Provide file locking (based on `flock`).
+class IrProxy::FileLock
+  autoload(:XDG, 'xdg')
+  autoload(:Digest, 'digest')
+  autoload(:FileUtils, 'fileutils')
+  autoload(:Pathname, 'pathname')
+
+  # @param [String] filepath
+  def initialize(filepath = nil)
+    @path = (filepath || default_filepath).to_s
+  end
+
+  # @return [String]
+  def to_s
+    self.path
+  end
+
+  def lock!(&block)
+    self.path.tap do |fp|
+      FileUtils.mkdir_p(Pathname.new(fp).dirname.to_s)
+      File.open(fp, File::CREAT).tap do |lock|
+        # returns false if already locked, 0 if not
+        lock.flock(File::LOCK_EX | File::LOCK_NB).tap do |ret|
+          # noinspection RubySimplifyBooleanInspection
+          return false == ret ? abort : block.call
+        end
+      end
+    end
+  end
+
+  protected
+
+  # @return [String]
+  attr_reader :path
+
+  # @return [String]
+  def default_filepath
+    "#{Sys::Proc.progname}/#{Digest::MD5.hexdigest(__FILE__)}.lock".tap do |fp|
+      return Pathname.new(XDG['CACHE'].to_s).join(fp).to_s
+    end
+  end
+
+  def abort
+    # rubocop:disable Style/RedundantException
+    raise RuntimeError, 'Already locked'
+    # rubocop:enable Style/RedundantException
+  end
+end
