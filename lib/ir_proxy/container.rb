211c0696eaf2234a9114d944cd238610e0bce85d
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-08-11 15:52:27 +0200

    container (lib) introduced

---
+++ lib/ir_proxy/container.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+# Copyright (C) 2017-2019 Dimitri Arrigoni <dimitri@arrigoni.me>
+# License GPLv3+: GNU GPL version 3 or later
+# <http://www.gnu.org/licenses/gpl.html>.
+# This is free software: you are free to change and redistribute it.
+# There is NO WARRANTY, to the extent permitted by law.
+
+require_relative '../ir_proxy'
+
+# Container for services (service locator).
+class IrProxy::Container
+  autoload(:Singleton, 'singleton')
+  include Singleton
+
+  # @return [Object]
+  def get(id)
+    dependencies.fetch(id.to_sym)
+  end
+
+  # @return [Boolean]
+  def has(id)
+    key?(id.to_sym)
+  end
+
+  # @return [self]
+  def set(id, instance)
+    self.tap do
+      dependencies.merge!(id.to_sym => instance)
+    end
+  end
+
+  # @return [Boolean]
+  def empty?
+    dependencies.empty?
+  end
+
+  def freeze
+    super.tap { dependencies.freeze }
+  end
+
+  protected
+
+  # @return [Hash{Symbol => Object}]
+  attr_accessor :dependencies
+
+  def initialize
+    @dependencies = {}
+  end
+end
