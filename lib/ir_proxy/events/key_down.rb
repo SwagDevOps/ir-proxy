7fe9a3c2029dfa8c2bd2a086e3fbaa01dbd8c639
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-09-03 08:42:00 +0200

    key_down (events) add logging

--- lib/ir_proxy/events/key_down.rb
+++ lib/ir_proxy/events/key_down.rb
@@ -8,15 +8,16 @@
 
 require_relative '../events'
 
-# Intercept and process lines
+# Intercept and process key down events
 class IrProxy::Events::KeyDown < IrProxy::Events::Listener
   # @param [IrProx::KeyScan] keyscan
   def call(keyscan)
-    pp(keyscan)
+    log_keyscan(keyscan)
   end
 
   def initialize(**kwargs)
     @adapter = kwargs[:adapter]
+    @logger = kwargs[:logger]
     (kwargs[:process_manager] || IrProxy[:process_manager]).tap do |pm|
       @process_manager = pm
     end
@@ -30,5 +31,23 @@ class IrProxy::Events::KeyDown < IrProxy::Events::Listener
   # @return [IrProxy::Adapter::Adapter]
   def adapter
     @adapter ||= IrProxy[:adapter]
+  end
+
+  # @return [IrProxy::Logger]
+  def logger
+    @logger ||= IrProxy[:logger]
+  end
+
+  # @param [IrProx::KeyScan] keyscan
+  #
+  # @return [self]
+  def log_keyscan(keyscan)
+    self.tap do
+      return self unless logger
+
+      Thread.new do
+        "key down #{keyscan.name}".tap { |s| logger.debug(s) }
+      end
+    end
   end
 end
