f39bbd2174334e93fe4bc7c64de1538a1ae3820c
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-09-03 08:29:04 +0200

    logger (lib) formatter changes

--- lib/ir_proxy/logger.rb
+++ lib/ir_proxy/logger.rb
@@ -11,4 +11,26 @@ require 'logger'
 
 # Provide logging facility.
 class IrProxy::Logger < ::Logger
+  attr_reader :progname
+
+  def initialize(*args, **kwargs)
+    super
+    @progname = kwargs[:progname]
+    self.formatter = make_formatter
+  end
+
+  protected
+
+  def make_formatter
+    # @formatter:off
+    proc do |severity, datetime, progname, msg|
+      [
+        datetime.strftime('%Y-%m-%d %H:%M:%S.%6N'),
+        severity[0],
+        "#{progname}[#{Process.pid}]:",
+        "#{msg}\n"
+      ].join(' ')
+    end
+    # @formatter:on
+  end
 end
