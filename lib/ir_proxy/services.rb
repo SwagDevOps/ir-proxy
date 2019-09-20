371b4a2374f0113fa4819a0ae131d8db9f62b3ba
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-09-20 08:41:14 +0200

    services (lib) removes useless require

--- lib/ir_proxy/services.rb
+++ lib/ir_proxy/services.rb
@@ -6,8 +6,6 @@
 # This is free software: you are free to change and redistribute it.
 # There is NO WARRANTY, to the extent permitted by law.
 
-require 'sys/proc'
-
 # @formatter:off
 {
   'progname': Sys::Proc.progname,
