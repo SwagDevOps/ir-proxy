41feacf1a9e8918f7761c1358480e39904ebe873
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-09-25 20:56:34 +0200

    cli (lib) add events_dispatcher dependency

--- lib/ir_proxy/cli.rb
+++ lib/ir_proxy/cli.rb
@@ -18,12 +18,24 @@ class IrProxy::Cli
   }.each { |s, fp| autoload(s, Pathname.new(__dir__).join("cli/#{fp}")) }
   # @formatter:on
 
+  def initialize(**kwargs)
+    @events_dispatcher = kwargs[:events_dispatcher]
+  end
+
   # Execute CLI.
   #
   # @param [Array<String>] given_args
   #
   # @return [void]
   def call(given_args = ARGV)
+    @events_dispatcher&.boot
+
     Command.start(given_args.clone)
+  end
+
+  protected
+
+  def events_dispatcher
+    @events_dispatcher || IrProxy[:events_dispatcher]
   end
 end
